b5a12fd863ede73fe16fa9dec3d147377a6a9220
Sam Wells <sfwells0518@gmail.com>
2023-03-31 12:38:42 -0500

    add index to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,13 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render :index
+    # render json: { message: "hello song index" }
+  end
+
+  def show
+    # @song = Song.find_by(id: params[:id])
+    # render :show
+    render json: { message: "hello song index" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/songs" => "songs#index"
-  # get "/songs/:id" => "songs#show"
+  get "/songs/:id" => "songs#show"
   # post "/songs" => "songs#create"
   # patch "/songs/:id" => "songs#update"
   # delete "/songs/:id" => "songs#destroy"
