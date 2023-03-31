6758eb62b0f6120127ad41e47d0340bd742ef43c
Sam Wells <sfwells0518@gmail.com>
2023-03-31 12:30:49 -0500

    add index REST route

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "/songs" => "songs#index"
+  # get "/songs/:id" => "songs#show"
+  # post "/songs" => "songs#create"
+  # patch "/songs/:id" => "songs#update"
+  # delete "/songs/:id" => "songs#destroy"
+end
