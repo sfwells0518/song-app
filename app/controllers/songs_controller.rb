81ab61a53e3f679d69c469a7cf8f2625c3af425a
Sam Wells <sfwells0518@gmail.com>
2023-03-31 13:44:48 -0500

    add update REST to controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,4 +22,15 @@ class SongsController < ApplicationController
     render :show
     # render json: { message: "hello created song" }
   end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.title = params[:title] || @song.title
+    @song.album = params[:album] || @song.album
+    @song.artist = params[:artist] || @song.artist
+    @song.year = params[:year] || @song.year
+    # render json: { message: "hello created song" }
+    @song.save
+    render :show
+  end
 end
